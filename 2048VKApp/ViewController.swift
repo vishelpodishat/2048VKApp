ab2e1fb38635c51fc90970177b8698ae6039623c
vishelpodishat <alishsaideshov@gmail.com>
2024-04-04 19:08:50 +0500

    Initial Commit

---
+++ 2048VKApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2048VKApp
+//
+//  Created by Alisher Saideshov on 04.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
