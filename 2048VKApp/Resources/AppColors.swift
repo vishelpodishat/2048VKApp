4b9c5ad88ab25ac6aa742ca9d6811b206ae809d7
vishelpodishat <alishsaideshov@gmail.com>
2024-04-04 22:27:18 +0500

    Colors initialize

---
+++ 2048VKApp/Resources/AppColors.swift
@@ -0,0 +1,8 @@
+//
+//  AppColors.swift
+//  2048VKApp
+//
+//  Created by Alisher Saideshov on 04.04.2024.
+//
+
+import Foundation
---
+++ 2048VKApp/Resources/Extensions/UIColor+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Extension.swift
+//  2048VKApp
+//
+//  Created by Alisher Saideshov on 04.04.2024.
+//
+
+import Foundation
